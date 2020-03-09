7e40f37bf93e1fa8a345c477684f1b095bc72eb9
Mark Ablovatskii <mark.ablov@gmail.com>
2020-03-09 15:04:59 +0700

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: ["jest"],
+  env: {
+    "jest/globals": true
+  },
+  globals: {
+    page: true,
+    browser: true,
+    context: true,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-throw-literal': 'off',
+    'no-await-in-loop': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'max-len': [
+      'error',
+      120,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+  },
+};
